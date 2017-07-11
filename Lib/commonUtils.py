87401098f278dca681eaa89b362bbec9673643ba
venkatrajulapati <venkata.rajulapati@gmail.com>
2017-07-12 00:35:54 +0530

    "test"

--- Lib/commonUtils.py
+++ Lib/commonUtils.py
@@ -41,38 +41,3 @@ def Get_UIObject(driver,Locator,Locatorvalue):
         Element = driver.find_element_by_xpath(Locatorvalue)
 
     return  Element
-def performAction(driver,objectClass,obj,valuetoSet):
-    if str(objectClass).lower()== "inputfield":
-        obj.send_keys(valuetoSet)
-
-    elif str(objectClass).lower() == "webelement":
-
-
-
-
-
-
-
-
-
-
-
-
-
-# driver = webdriver.Chrome()
-# driver.get()
-# ele =driver.find_element_by_xpath(".//td[contains(text(),'Access')]/parent::tr/td[3]/select")
-# select = Select(ele)
-# select.select_by_value("disabled")
-
-#
-# driver.maximize_window()
-#
-# driver.get("https://www.google.co.in")
-#
-# time.sleep(1)
-#
-# elem = driver.find_element_by_id("lst-ib")
-#
-# elem.send_keys("Python")
-# elem.send_keys(Keys.ENTER)
