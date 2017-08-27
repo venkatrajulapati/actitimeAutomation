913b99ca39ef1e609d23ba72a2c081df86c5a128
venkatrajulapati <venkata.rajulapati@gmail.com>
2017-08-27 19:42:06 +0530

    "New test case"

--- Lib/commonUtils.py
+++ Lib/commonUtils.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
 import time
+import xlrd
 
 def Get_Browser(BrName):
     BrName = str(BrName)
@@ -41,3 +42,9 @@ def Get_UIObject(driver,Locator,Locatorvalue):
         Element = driver.find_element_by_xpath(Locatorvalue)
 
     return  Element
+
+
+oWb = xlrd.open_workbook('C:\\Users\\RAJULAPATI\\Desktop\\ALMMacros\\ALM_Connection2.xls').get_sheet(2)
+owb.
+
+
--- TestPlan/Tc1.py
+++ TestPlan/Tc1.py
@@ -8,7 +8,7 @@ driver = Get_Browser('chrome')
 openApplication(driver,"https://www.google.co.in/")
 
 elem = Get_UIObject(driver,'id','lst-ib')
-performAction(elem,"python")
+#performAction(elem,"python")
 
 elem.send_keys("python")
 elem.send_keys(Keys.ENTER)
